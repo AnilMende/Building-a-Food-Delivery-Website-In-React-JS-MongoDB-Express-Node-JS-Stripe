09aa0eefe4f1ed35d435c72ccd3620c70dd8933a
Mende Anil Kumar <anilmende2002@gmail.com>
2025-11-08 01:20:47 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,11 +6,12 @@ dotenv.config();
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-    const frontend_url = "https://food-del-frontend-u6qi.onrender.com"
+    // const frontend_url = "https://food-del-frontend-u6qi.onrender.com"
 // placing user order for frontend
 const placeOrder = async (req, res) => {
 
     // const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://food-del-frontend-u6qi.onrender.com"
 
     try{
         const {items, amount, address, paymentMethod} = req.body;

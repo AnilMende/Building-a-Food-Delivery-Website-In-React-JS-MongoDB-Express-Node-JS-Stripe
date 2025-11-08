76534a7cfa962bc0f4ead8d17643cd4118cd6ff9
Mende Anil Kumar <anilmende2002@gmail.com>
2025-11-08 23:53:37 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
 // export const url = 'http://localhost:4000'
-export const url = 'https://food-del-backend-pflt.onrender.com';
+export const url = "https://food-del-backend-88wj.onrender.com";
 
 export const assets ={
     logo,
